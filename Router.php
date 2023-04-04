7bfb7999ec583eba6a40d8f238b998c8906081da
maibarra0904 <mario.ibarra.86@gmail.com>
2023-04-03 19:56:14 -0500

    030423

--- Router.php
+++ Router.php
@@ -24,7 +24,7 @@ class Router {
         //Arreglo de rutas protegidas..
         $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
 
-        $urlActual = $_SERVER['REQUEST_URI']=== '/' ? '/' : $_SERVER['PATH_INFO'];
+        $urlActual = $_SERVER['REQUEST_URI']=== '/' ? '/' : $_SERVER['REQUEST_URI'];
         
         $metodo = $_SERVER['REQUEST_METHOD'];
         
